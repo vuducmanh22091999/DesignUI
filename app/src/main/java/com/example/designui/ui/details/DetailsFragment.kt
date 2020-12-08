09040b9291b2e290f554a2db3d685fc73b968537
DESKTOP-FVVNTE1\DELL LATITUDE E5450 <manhwanda@gmail.com>
2020-12-08 17:03:55 +0700

    - Feat (details) : design UI.

---
+++ app/src/main/java/com/example/designui/ui/details/DetailsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.designui.ui.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.designui.R
+
+class DetailsFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/designui/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/designui/ui/home/HomeFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.designui.R
+import com.example.designui.ui.details.DetailsFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
     private lateinit var statusAdapter: StatusAdapter
     private var listStatus: ArrayList<Status> = ArrayList()
 
@@ -28,7 +30,17 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initRecyclerView()
+        initListener()
         getDate()
+    }
+
+    private fun initListener() {
+        tvOverview.setOnClickListener(this)
+    }
+
+    private fun switchDetails() {
+        requireActivity().supportFragmentManager.beginTransaction().add(R.id.flContentScreen, DetailsFragment())
+            .addToBackStack(null).commit()
     }
 
     private fun getDate() {
@@ -39,14 +51,42 @@ class HomeFragment : Fragment() {
 
     private fun initRecyclerView() {
         listStatus.add(Status(R.drawable.ic_up_arrow, "Sent", "Sending Payment to Clients", "$150"))
-        listStatus.add(Status(R.drawable.ic_down_arrow, "Receive", "Receiving Salary from Company", "$250"))
+        listStatus.add(
+            Status(
+                R.drawable.ic_down_arrow,
+                "Receive",
+                "Receiving Salary from Company",
+                "$250"
+            )
+        )
         listStatus.add(Status(R.drawable.ic_loan, "Loan", "Loan for the car", "$400"))
-        listStatus.add(Status(R.drawable.ic_down_arrow, "Receive", "Receiving Salary from Company", "$550"))
+        listStatus.add(
+            Status(
+                R.drawable.ic_down_arrow,
+                "Receive",
+                "Receiving Salary from Company",
+                "$550"
+            )
+        )
         listStatus.add(Status(R.drawable.ic_up_arrow, "Sent", "Sending Payment to Clients", "$150"))
         listStatus.add(Status(R.drawable.ic_loan, "Loan", "Loan to Clients", "$150"))
-        listStatus.add(Status(R.drawable.ic_down_arrow, "Receive", "Receiving Salary from Company", "$750"))
+        listStatus.add(
+            Status(
+                R.drawable.ic_down_arrow,
+                "Receive",
+                "Receiving Salary from Company",
+                "$750"
+            )
+        )
         listStatus.add(Status(R.drawable.ic_up_arrow, "Sent", "Sending Payment to Clients", "$150"))
-        listStatus.add(Status(R.drawable.ic_down_arrow, "Receive", "Receiving Salary from Company", "$1950"))
+        listStatus.add(
+            Status(
+                R.drawable.ic_down_arrow,
+                "Receive",
+                "Receiving Salary from Company",
+                "$1950"
+            )
+        )
         listStatus.add(Status(R.drawable.ic_loan, "Loan", "Loan for the car", "$900"))
         listStatus.add(Status(R.drawable.ic_up_arrow, "Sent", "Sending Payment to Clients", "$150"))
         listStatus.add(Status(R.drawable.ic_loan, "Loan", "Loan for the car", "$1500"))
@@ -56,6 +96,12 @@ class HomeFragment : Fragment() {
         rcvStatus.layoutManager = linearLayoutManager
         rcvStatus.setHasFixedSize(true)
         rcvStatus.adapter = statusAdapter
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.tvOverview -> switchDetails()
+        }
     }
 
 
